3ba940feb0ff3fb1dcb5e9a3b7570382a02a0fee
Julián Eduardo Mena Chávez <69028614+JulinkG@users.noreply.github.com>
2024-04-07 22:56:42 -0600

    Update ANAGRAMA.py

--- Scripts1_20/ANAGRAMA.py
+++ Scripts1_20/ANAGRAMA.py
@@ -2,7 +2,7 @@
 # * verdadero o falso (Bool) según sean o no anagramas.
 # * - Un Anagrama consiste en formar una palabra reordenando TODAS
 # *   las letras de otra palabra inicial.
-# * - NO hace falta comprobar que ambas palabras existan.
+# * - NO hace falta comprobar que ambas palabras existan
 # * - Dos palabras exactamente iguales no son anagrama.
 
 def anagrama(palabra1,palabra2):
